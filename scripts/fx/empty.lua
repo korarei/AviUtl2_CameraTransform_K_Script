d54d3ecf2fb48c890e7e624613c6126e764ce939
Korarei <132204522+korarei@users.noreply.github.com>
2026-06-28 04:28:54 +0900

    Feat: Add transform parameters to empty object

--- scripts/fx/empty.lua
+++ scripts/fx/empty.lua
@@ -4,6 +4,24 @@
 --information:Empty@${SCRIPT_NAME} v${PROJECT_VERSION} by ${PROJECT_AUTHOR}
 --label:${LABEL}
 
+--group:Position,true
+local position_x = 0.0 --track@position_x:Position::X,-100000,100000,0,0.01
+local position_y = 0.0 --track@position_y:Position::Y,-100000,100000,0,0.01
+local position_z = 0.0 --track@position_z:Position::Z,-100000,100000,0,0.01
+--trackgroup@position_x,position_y,position_z:Group::Position
+--group:Rotation,true
+local rotation_w = 0.0 --track@rotation_w:Rotation::W,-3600,3600,0,0.01
+local rotation_x = 0.0 --track@rotation_x:Rotation::X,-3600,3600,0,0.01
+local rotation_y = 0.0 --track@rotation_y:Rotation::Y,-3600,3600,0,0.01
+local rotation_z = 0.0 --track@rotation_z:Rotation::Z,-3600,3600,0,0.01
+--#define EULER XYZ Euler=5,XZY Euler=7,YXZ Euler=11,YZX Euler=15,ZXY Euler=19,ZYX Euler=21
+local rotation_mode = 21 --select@rotation_mode:Rotation::Mode=21,Quaternion=0,Axis Angle=1,${EULER}
+--trackgroup@rotation_x,rotation_y,rotation_z:Group::Rotation
+--group:Scale,true
+local scale_x = 100.0 --track@scale_x:Scale::X,0,10000,100,0.01
+local scale_y = 100.0 --track@scale_y:Scale::Y,0,10000,100,0.01
+local scale_z = 100.0 --track@scale_z:Scale::Z,0,10000,100,0.01
+--trackgroup@scale_x,scale_y,scale_z:Group::Scale
 --group:Relations,true
 local relations_parent_layer = 0 --track@relations_parent_layer:Relations::Parent Layer,-100,100,0,1,---
 --group:Visibility,true
@@ -35,6 +53,10 @@ do
     local kCacheImage = "cache:ad0476bd-2eaa-4852-80a6-5feda1dc7587-" .. obj.effect_id
     local kEpsilon = 1.0e-4
 
+    scale_x = scale_x * 0.01
+    scale_y = scale_y * 0.01
+    scale_z = scale_z * 0.01
+
     influence = influence * 0.01
 
     if layer_reference == 0 then
@@ -64,12 +86,28 @@ do
         vector.reset()
     end
 
+    vector.compose(
+        influence,
+        position_x,
+        position_y,
+        position_z,
+        rotation_w,
+        rotation_x,
+        rotation_y,
+        rotation_z,
+        rotation_mode,
+        scale_x,
+        scale_y,
+        scale_z
+    )
+
     global[kKeyXform] =
         buffer.encode({ influence, obj.ox, obj.oy, obj.oz, obj.rx, obj.ry, obj.rz, obj.sx, obj.sy, obj.sz })
 
     if obj.getinfo("saving") and not visibility_show_in_renders or not visibility_show_in_viewports then
         obj.clearbuffer("object", 1, 1, 0)
-        obj.alpha = 0.01
+        obj.alpha = kEpsilon
+        obj.setoption("focus_mode", "fixed_size")
     else
         local x, y, z = obj.getvalue("pos")
         local rx, ry, rz = obj.getvalue("angle")
